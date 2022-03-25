859d651152a5c47b00869e0650611144f62c25a7
rahul unni <rahul@rahulsmac.local>
2022-03-25 20:23:58 +0530

    UC1 added

---
+++ SnakeLadder/UC1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SnakeLadder
+{
+    public class UC1
+    {
+        public UC1()
+        {
+            Console.WriteLine("Welcome to UC1");
+            Console.WriteLine("Snake and ladder with single player start at 0");
+            int start = 0;
+
+        }
+    }
+}
