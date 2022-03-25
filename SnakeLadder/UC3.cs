805366dc728f7a55e3b98656cea13cf2783349d8
rahul unni <rahul@rahulsmac.local>
2022-03-25 20:25:55 +0530

    UC3 added

---
+++ SnakeLadder/UC3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace SnakeLadder
+{
+    public class UC3
+    {
+        public UC3()
+        {
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            Console.WriteLine("Welcome to UC3");
+            Random random = new Random();
+            int randomCheck = random.Next(3);
+
+            switch (randomCheck)
+            {
+                case 1:
+                    {
+                        Random rnd = new Random();
+                        ldice = rnd.Next(1, 7);
+                        Console.WriteLine("Got Ladder");
+                        break;
+                    }
+                case 2:
+                    {
+                        Random sran = new Random();
+                        sdice = sran.Next(1, 7);
+                        Console.WriteLine("Got snake");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("No play, Stay at base");
+                        break;
+                    }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
